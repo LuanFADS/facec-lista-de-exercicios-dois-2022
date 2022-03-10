5b93d45e51b09ffcde93edb684e828e8cc9041b4
Luan Francisco <luancianorte@gmail.com>
2022-03-09 22:21:08 -0300

    Initial commit

---
+++ ResolucaoEx02/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucaoEx02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a operação matemática: ");
+            char op = ' ';
+           
+            do
+            {   
+                try
+                {
+                    op = char.Parse(Console.ReadLine());
+                    if (op != '+' || op != '-' || op != '*' || op != '/')
+                    {
+                        throw new Exception("Informe uma operação matemática de verdade entre [" +
+                            " + | - | * | / ]");
+                    }
+                } 
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                } 
+                //finally 
+                //{ 
+                //    Console.ReadLine(); 
+                //}    
+            } while (op != '+' || op != '-' || op != '*' || op != '/');
+
+            Console.WriteLine("Informe o primeiro valor da conta: ");
+            double value01 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo valor da conta: ");
+            double value02 = double.Parse(Console.ReadLine());
+                    
+            switch (op)
+            { 
+                case '+': 
+                    Console.WriteLine("Resultado: " + $"{value01 + value02}");
+                    break;
+                case '-': 
+                    Console.WriteLine("Resultado: " + $"{value01 - value02}");
+                    break;
+                case '*': 
+                    Console.WriteLine("Resultado: " + $"{value01 * value02}");
+                    break;
+                case '/': 
+                    Console.WriteLine("Resultado: " + $"{value01 / value02}");
+                    break;
+                default:
+                    throw new Exception("Não tem nem como chegar aqui!");
+                    break;      
+            }
+            Console.ReadKey();   
+
+        }
+    }
+}
---
+++ ResolucaoEx03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolucaoEx03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o seu nome: ");
+            String nome = Console.ReadLine();
+            int y = 0;
+            int z = 0;
+
+            for (int i = 0; i == nome.Length - 1; i++)
+            {
+                if (nome.Substring(i) == "a" || nome.Substring(i) == "e" || nome.Substring(i) == "i"
+                    || nome.Substring(i) == "o" || nome.Substring(i) == "u")
+                {
+                    y++;
+                }
+                else
+                {
+                    z++;
+                }     
+            }
+            Console.WriteLine($"Existe(m) {y} vogal(is) da palavra ({nome})");
+            Console.WriteLine($"Existe(m) {z} consoante(s) da palavra ({nome})");
+            Console.ReadLine();
+        }
+    }
+}
